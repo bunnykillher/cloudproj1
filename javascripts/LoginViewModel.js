95e36fcb098ae1b083dd4ef3075ee9edbbe84c36
bunnykillher <bunnykillher@gmail.com>
2016-02-01 19:43:48 +0800

    Update LoginViewModel.js

--- javascripts/LoginViewModel.js
+++ javascripts/LoginViewModel.js
@@ -15,6 +15,7 @@ var LoginViewModel = function(makeLoginViewVisible) {
 	    	alert("LOGIN SUCCESSFUL. Create a new AppViewModel for your app");
 	  	} else {
 	    	// user is logged out
+	    	ViewModels.appVM.isVisible(true);
 			console.log("User logged out");
 	  	}
 	});
